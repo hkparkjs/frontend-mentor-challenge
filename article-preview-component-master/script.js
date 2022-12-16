eb48f408ed20f5bb76f7905ee035744968ba86fe
Hyeon Park <hkparkjs@gmail.com>
2022-12-16 23:59:51 +0900

    🐛 Modify popup error

--- article-preview-component-master/script.js
+++ article-preview-component-master/script.js
@@ -1,34 +1,9 @@
+"use strict";
+
 const popup = document.querySelector('.popup-share-box');
 const shareBtn = document.querySelector('.share');
-const popupShareBtn = document.querySelector('.article-footer button');
-let isShow = false;
 
-function adjustPopupLocation() {
-  popup.style.top = `${shareBtn.getBoundingClientRect().top - 85}px`;
-  popup.style.left = `${shareBtn.getBoundingClientRect().left - 105}px`;
-}
-
-shareBtn.addEventListener('click', () => {
-  if (!isShow) {
-    popup.style.display = "flex";
-    isShow = true;
-
-    if (screen.width >= 768) {
-      adjustPopupLocation();
-      popupShareBtn.classList.add('share-dark');
-      popupShareBtn.classList.remove('share-light');
-    }    
-  } else {
-    popup.style.display = "none";
-    isShow = false;
-    popupShareBtn.classList.add('share-light');
-    popupShareBtn.classList.remove('share-dark');
-  }
+shareBtn.addEventListener("click", () => {
+  popup.classList.toggle("show");
+  shareBtn.classList.toggle("active");
 });
-
-window.addEventListener('resize', () => {
-  if (isShow && screen.width >= 768) {
-    adjustPopupLocation();
-  }
-});
-
