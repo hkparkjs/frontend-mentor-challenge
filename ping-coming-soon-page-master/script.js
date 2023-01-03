2e2b1acadc6e38d670fc620588e75530efb23167
Hyeon Park <hkparkjs@gmail.com>
2023-01-03 17:54:58 +0900

    🐛 Add error msg and adjust styles

--- ping-coming-soon-page-master/script.js
+++ ping-coming-soon-page-master/script.js
@@ -2,16 +2,20 @@
 
 const submitBtn = document.querySelector('.submit-btn');
 const subscription = document.querySelector('.subscription');
-const emailTxt = document.querySelector('.email-input input'); 
+const emailTxt = document.querySelector('.email-input input');
+const errorMsg = document.querySelector('.error-msg');
 
 function checkEmailAddress(email) {
   // 이메일주소가 유효하지 않으면 에러출력.
   // 입력값 없으면
   const regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
   // const regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
-  if (!regex.test(email)) {
+  if (email === '') {
+    errorMsg.innerText = "Whoops! It looks like you forgot to add your email";
     return false;
-  } else if (email === '') {
+  }
+  if (!regex.test(email)) {
+    errorMsg.innerText = "Please provide a valid email address";
     return false;
   }
   return true;
