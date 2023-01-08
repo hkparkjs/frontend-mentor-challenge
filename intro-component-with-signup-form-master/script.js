7e48267f6b421abbdd8049c9b15098075b7d5ddc
hyeon Park <hkparkjs@gmail.com>
2023-01-08 11:35:58 +0900

    👔  Change checking email logic handling UI when occuring invalid event

--- intro-component-with-signup-form-master/script.js
+++ intro-component-with-signup-form-master/script.js
@@ -5,23 +5,26 @@ const errorMsg = document.getElementsByClassName('error-msg');
 const form = document.getElementsByClassName('signup-form');
 
 function checkEmailAddress(email) {
-  const regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
-
-  if (!regex.test(email)) {
-    return false;
+  // http://emailregex.com/
+  let regex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  let result = false;
+  if (regex.test(email)) {
+    result = true;
   } else if (email === '') {
-    return false;
+    result = false;
   }
-  return true;
+  return result;
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   let err = false;
   for (let i=0; i<inputField.length; i++) {
+    // initialization
     if (inputItem[i].classList.contains('error')) {
       inputItem[i].classList.remove('error');
     }
+
     if (inputField[i].children[1].value === '') {
       err = true;
       let field = inputField[i].children[1].placeholder;
@@ -29,8 +32,8 @@ function handleSubmit(event) {
       inputItem[i].classList.add("error");
     }
 
-    let email = document.getElementById("email"); 
-
+    // email validation
+    let email = document.getElementById("email");
     if (email.value && !checkEmailAddress(email.value)) {
       err = true;
       errorMsg[2].children[0].innerText = 'Looks like this is not an email';
@@ -38,6 +41,7 @@ function handleSubmit(event) {
     }
   }
 
+  // claim submit action and clear
   if (err == false) {
     alert("claim finished!");
     for (let i=0; i<inputField.length; i++) {
@@ -48,10 +52,26 @@ function handleSubmit(event) {
   }
 }
 
+/* form submit event */
 form[0].addEventListener('submit', handleSubmit);
 
+// handling invalid event (browser default event)
+inputField[2].children[1].addEventListener('invalid', e => {
+  e.preventDefault();
+  errorMsg[2].children[0].innerText = 'Looks like this is not an email';
+  inputItem[2].classList.add("error");
+  // call for UI because the submit event isn't occured when the invalid event is issued
+  handleSubmit(e);
+});
+
+// remove err style when user click input field again after error or input field is changed
 for(let i=0; i<inputField.length; i++) {
   inputField[i].children[1].addEventListener('click', function () {
+    if (inputItem[i].classList.contains('error')) {
+      inputItem[i].classList.remove('error');
+    }
+  });
+  inputField[i].children[1].addEventListener('change', function () {
     if (inputItem[i].classList.contains('error')) {
       inputItem[i].classList.remove('error');
     }
