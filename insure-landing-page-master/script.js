36d334c753b83f0e39deaa14bc0b795fac5d2c95
hyeon Park <hkparkjs@gmail.com>
2023-01-29 12:06:52 +0900

    🐛 Remove horizental scroll

--- insure-landing-page-master/script.js
+++ insure-landing-page-master/script.js
@@ -2,13 +2,16 @@
 
 const menuBtn = document.querySelector('.menu-btn');
 const nav = document.querySelector('nav');
+const body = document.querySelector('body');
 
 menuBtn.addEventListener('click', () => {
   if (menuBtn.classList.contains('active')) {
     menuBtn.classList.remove('active');
     nav.classList.remove('active');
+    body.style.overflow = "visible";
   } else {
     menuBtn.classList.add('active');
     nav.classList.add('active');
+    body.style.overflow = "hidden";
   }
 });
